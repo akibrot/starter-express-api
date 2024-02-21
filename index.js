38077beaefbcdeeefd62e65d4fc188bf26cba865
akibrot <akibrotsamuelas@gmail.com>
2024-02-20 23:05:56 -0800

    cors modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port=process.env.PORT || 3001
 const app=express()
 app.use(express.json({limit:"50mb"}))
 // app.use(cors())
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 app.listen(port,()=>{
     console.log("app started in port "+port)
